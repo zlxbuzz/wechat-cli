a08a26be392ec0aad65a9a75322e29aa3554e24c
zlxbuzz <824665478@qq.com>
2017-04-14 15:56:14 +0800

    新增await

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 //获取应用实例
 var app = getApp()
+//await 暂时需要这样
+const regeneratorRuntime = require('../../utils/runtime.js')
 Page({
   data: {
     motto: 'Hello World',
@@ -12,10 +14,16 @@ Page({
       url: '../logs/logs'
     })
   },
+  async getData(){
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+    await app.ajaxGet('matchH5/detail',{matchID: 3998713102401616});
+  },
   onLoad: function () {
-    app.ajaxGet('matchH5/detail',{matchID: 3998713102401616}).then((res)=>{
-      console.log(res);
-    });
+    this.getData();
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
